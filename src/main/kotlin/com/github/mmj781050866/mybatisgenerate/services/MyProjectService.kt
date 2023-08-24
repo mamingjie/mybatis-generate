bf5a15e24417cbc8471cb356de22a0a94ca1415f
GitHub Action <action@github.com>
2023-08-24 03:26:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/mmj781050866/mybatisgenerate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mmj781050866/mybatisgenerate/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.mmj781050866.mybatisgenerate.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mmj781050866.mybatisgenerate.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
